d8e60f476050ff136272e7773aab63e31ab06fb5
renton18 <gakituka18@gmail.com>
2018-06-28 22:14:10 +0900

    commit 2

--- DataGridViewTest/Form1.Designer.cs
+++ DataGridViewTest/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.CLBtest_tb = new System.Windows.Forms.Button();
             this.CLBSet_bt = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -162,11 +163,21 @@
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(361, 195);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 467);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.CLBSet_bt);
             this.Controls.Add(this.CLBtest_tb);
@@ -204,6 +215,7 @@
         private System.Windows.Forms.Button CLBtest_tb;
         private System.Windows.Forms.Button CLBSet_bt;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 

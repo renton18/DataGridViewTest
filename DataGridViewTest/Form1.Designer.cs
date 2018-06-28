daf5836beaa26e0011149b2309f3b505f3b356f3
renton18 <gakituka18@gmail.com>
2018-06-28 22:42:51 +0900

    commit

--- DataGridViewTest/Form1.Designer.cs
+++ DataGridViewTest/Form1.Designer.cs
@@ -40,8 +40,6 @@
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.CLBtest_tb = new System.Windows.Forms.Button();
             this.CLBSet_bt = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -154,31 +152,11 @@
             this.CLBSet_bt.UseVisualStyleBackColor = true;
             this.CLBSet_bt.Click += new System.EventHandler(this.CLBSet_bt_Click);
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(361, 166);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 12;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(361, 195);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 13;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 467);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.CLBSet_bt);
             this.Controls.Add(this.CLBtest_tb);
             this.Controls.Add(this.checkedListBox1);
@@ -214,8 +192,6 @@
         private System.Windows.Forms.CheckedListBox checkedListBox1;
         private System.Windows.Forms.Button CLBtest_tb;
         private System.Windows.Forms.Button CLBSet_bt;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
 
